4f6f2d686bf1630fac6a9eae50d81fb7d882141f
super-sox <micahpope6@gmail.com>
2024-05-03 22:45:04 -0500

    Cleaned up syntax a bit

--- directory-cleaner.py
+++ directory-cleaner.py
@@ -3,29 +3,31 @@ import os
 
 
 def get_file_types(files):
-    #get list of unique file types in directory
+    # get list of unique file types in directory
     file_types = []
     for file in files:
         name_type = file.lower().split(".")[1]
-        if not name_type in file_types:
+        if name_type not in file_types:
             file_types += [name_type]
         else:
             pass
     return file_types
 
+
 def create_file_dir(file_types, target_path):
-    #create a directory for each file type
+    # create a directory for each file type
     new_dir_paths = {}
     for file_type in types:
         new_dir_path = str(path + file_type + "_files/")
         new_dir_paths[file_type] = new_dir_path
-        #only create directory if it does not already exist
+        # only create directory if it does not already exist
         try:
             os.mkdir(new_dir_path)
             print(f"{file_type + '_files'} created!")
         except OSError:
             print(f"Skipping {file_type + ' files'}! {new_dir_path} already exists!")
     return new_dir_paths
+
 
 def move_files(file_type_dirs, file_list, undo=False):
     for file_type in file_type_dirs:
@@ -51,10 +53,10 @@ while True:
         except OSError:
             print("Error! Directory does not exist or is not accessible!")
 
-#pull list of files from directory contents
+# pull list of files from directory contents
 files = []
-for item in dir_contents: 
-    #currently excludes files with more than one . in their name, such as hidden files and temporary files (i.e swap files)
+for item in dir_contents:
+    # currently excludes files with more than one . in their name, such as hidden files and temporary files (i.e swap files)
     if os.path.isfile(path + item) and len(item.split(".")) <= 2 and item != "directory-cleaner.py":
         files += [item]
 
@@ -72,7 +74,7 @@ print(os.listdir(path))
 
 while True:
     undo_y_n = input("Undo? y/n : ")
-    if undo_y_n  == "y":
+    if undo_y_n == "y":
         move_files(created_dir_paths, files, True)
         for file_type in created_dir_paths:
             os.rmdir(created_dir_paths[file_type])
@@ -83,4 +85,3 @@ while True:
         break
     else:
         print("Error! Response not recognized!")
-    
